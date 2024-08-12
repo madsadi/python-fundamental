e3a57f05f959c845b736896b675090c92f76d7fa
madsadi <sadixus@gmail.com>
2024-08-12 17:57:44 +0200

    calculating the AQI and write them in cloud

--- Assignment2.py
+++ Assignment2.py
@@ -10,6 +10,11 @@ import numpy as np
 channel_id=343018
 channel = thingspeak.Channel(id=channel_id)
 
+my_channel_id=2624254
+write_api_key = 'WDPU0U3DEW5GBPC2'
+read_api_key = 'Q76IR3KHD2RV4Q2A'
+my_channel = thingspeak.Channel(id=my_channel_id, api_key=write_api_key)
+
 RPI.setmode(RPI.BCM) # --> setting numbering system for GPIOs
 my_dht11 = dht11.DHT11(pin=4)  # --> creating an instance for DHT11 usage
 
@@ -92,13 +97,107 @@ def update_buffer(new_measurement):
     buffer_control(temp,temp_value)
     buffer_control(humidity,humidity_value)
 
+def calculate_AQI(measurement,value):
+    AQI = 0
+    categories=[
+            {
+                'title':'GOOD',
+                'min_PM2.5':0,
+                'max_PM2.5':12,
+                'min_PM10': 0,
+                'max_PM10': 54,
+                'min_index':0,
+                'max_index':50,
+            },
+            {
+                'title': 'MODERATE',
+                'min_PM2.5': 12.1,
+                'max_PM2.5': 35.4,
+                'min_PM10': 55,
+                'max_PM10': 154,
+                'min_index': 51,
+                'max_index': 100,
+            },
+            {
+                'title': 'UNHEALTHY FOR SENSITIVE GROUPS',
+                'min_PM2.5': 35.5,
+                'max_PM2.5': 55.4,
+                'min_PM10': 155,
+                'max_PM10': 254,
+                'min_index': 101,
+                'max_index': 150,
+            },
+            {
+                'title': 'UNHEALTHY',
+                'min_PM2.5': 55.5,
+                'max_PM2.5': 150.4,
+                'min_PM10': 255,
+                'max_PM10': 354,
+                'min_index': 151,
+                'max_index': 200,
+            },
+            {
+                'title': 'VERY UNHEALTHY',
+                'min_PM2.5': 150.5,
+                'max_PM2.5': 250.4,
+                'min_PM10': 355,
+                'max_PM10': 424,
+                'min_index': 201,
+                'max_index': 300,
+            },
+            {
+                'title': 'HAZARDOUS',
+                'min_PM2.5': 250.5,
+                'max_PM2.5': 350.4,
+                'min_PM10': 425,
+                'max_PM10': 504,
+                'min_index': 301,
+                'max_index': 400,
+            },
+            {
+                'title': 'HAZARDOUS',
+                'min_PM2.5': 350.5,
+                'max_PM2.5': 500.4,
+                'min_PM10': 505,
+                'max_PM10': 604,
+                'min_index': 401,
+                'max_index': 500,
+            },
+        ]
+    if measurement=='PM2.5':
+        for category in categories:
+            if value>=category['min_PM2.5'] and value<=category['max_PM2.5']:
+                AQI=(((category['max_index']-category['min_index'])/(category['max_PM2.5']-category['min_PM2.5']))*(value-category['min_PM2.5'])) + category['min_index']
+    elif measurement=='PM10':
+        for category in categories:
+            if value>=category['min_PM10'] and value<=category['max_PM10']:
+                AQI=(((category['max_index']-category['min_index'])/(category['max_PM10']-category['min_PM10']))*(value-category['min_PM10'])) + category['min_index']
+    return AQI
+
+def AQI():
+    fields = get_each_field()
+    ten_AQI = []
+    for value in fields['field1']:
+        ten_AQI.append(calculate_AQI('PM10', value))
+
+    two_point_five_AQI = []
+    for value in fields['field3']:
+        two_point_five_AQI.append(calculate_AQI('PM2.5', value))
+
+    max_AQI = np.amax(np.array([ten_AQI, two_point_five_AQI]), axis=0)
+
+    # for i in range(len(max_AQI)):
+    my_channel.update({'field1': str(two_point_five_AQI),'field2': str(ten_AQI),'field3': str(max_AQI)})
+
+AQI()
+
 while True:
     temp_humidity_read = my_dht11.read()  # -> getting a single sensor measurement
-
-    while not temp_humidity_read.is_valid():
-        temp_humidity_read = my_dht11.read()  # -> getting a single sensor measurement
-
-    update_buffer(temp_humidity_read)
-    plot(temp,'Temperature (C)','green','dotted')
-    plot(humidity,'Humidity','navy','dotted')
+    #
+    # while not temp_humidity_read.is_valid():
+    #     temp_humidity_read = my_dht11.read()  # -> getting a single sensor measurement
+    #
+    # update_buffer(temp_humidity_read)
+    # plot(temp,'Temperature (C)','green','dotted')
+    # plot(humidity,'Humidity','navy','dotted')
 
